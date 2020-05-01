373764effa02a14b69ec79c157e96b5038b82703
M. Fadli Zein <m.fadli.zein.17@gmail.com>
2020-05-01 15:29:29 +0700

    full material

--- app/src/main/java/com/gzeinnumer/basestyle/ThirdActivity.kt
+++ app/src/main/java/com/gzeinnumer/basestyle/ThirdActivity.kt
@@ -11,45 +11,10 @@ import android.widget.ArrayAdapter
 import android.widget.PopupMenu
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.res.ResourcesCompat
-import com.tiper.MaterialSpinner
 import kotlinx.android.synthetic.main.activity_main.*
 
 
 class ThirdActivity : AppCompatActivity() {
-
-    private val listener by lazy {
-        object : MaterialSpinner.OnItemSelectedListener {
-            override fun onItemSelected(
-                parent: MaterialSpinner,
-                view: View?,
-                position: Int,
-                id: Long
-            ) {
-                Log.v("MaterialSpinner", "onItemSelected parent=${parent.id}, position=$position")
-                when (parent) {
-                    material_spinner_1 -> {
-                        Log.v("MaterialSpinner", "material_spinner_1")
-                    }
-                    material_spinner_2 -> {
-                        Log.v("MaterialSpinner", "material_spinner_2")
-                    }
-                    material_spinner_3 -> {
-                        Log.v("MaterialSpinner", "material_spinner_3")
-                    }
-                    material_spinner_4 -> {
-                        Log.v("MaterialSpinner", "material_spinner_4")
-                    }
-                    material_spinner_5 -> {
-                        Log.v("MaterialSpinner", "material_spinner_5")
-                    }
-                }
-            }
-
-            override fun onNothingSelected(parent: MaterialSpinner) {
-                Log.v("MaterialSpinner", "onNothingSelected parent=${parent.id}")
-            }
-        }
-    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -67,48 +32,14 @@ class ThirdActivity : AppCompatActivity() {
             }
             false
         }
-        ArrayAdapter.createFromResource(
-            this,
-            R.array.planets_array,
-            R.layout.mygzn_simple_spinner_item
-        ).let {
-            it.setDropDownViewResource(R.layout.mygzn_spinner_dropdown_item)
-            material_spinner_1.apply {
-                adapter = it
-                onItemSelectedListener = listener
-                onFocusChangeListener = View.OnFocusChangeListener { v, hasFocus ->
-                    Log.v("MaterialSpinner", "onFocusChange hasFocus=$hasFocus")
-                }
-            }
-            material_spinner_2.apply {
-                adapter = it
-                onItemSelectedListener = listener
-            }
-            material_spinner_3.apply {
-                adapter = it
-                onItemSelectedListener = listener
-                selection = 3
-                setDrawable(
-                    ResourcesCompat.getDrawable(
-                        resources,
-                        R.drawable.mygzn_arrow_downward,
-                        theme
-                    )
-                )
-            }
-        }
-        ArrayAdapter.createFromResource(
-            this,
-            R.array.planets_array,
-            R.layout.mygzn_simple_spinner_item
-        ).let {
-            it.setDropDownViewResource(R.layout.mygzn_spinner_dropdown_item)
-            material_spinner_4.adapter = it
-            material_spinner_5.adapter = it
-        }
-        material_spinner_1.let {
 
-        }
+//        val items = listOf("Material", "Design", "Components", "Android")
+        val items = resources.getStringArray(R.array.planets_array)
+        val myAdapter = ArrayAdapter(applicationContext, R.layout.mygzn_list_item, items)
+        material_spinner_1.setAdapter(myAdapter)
+        material_spinner_2.setAdapter(myAdapter)
+        material_spinner_4.adapter = myAdapter
+        material_spinner_5.adapter = myAdapter
     }
 
     fun showPopup(v: View?) {
